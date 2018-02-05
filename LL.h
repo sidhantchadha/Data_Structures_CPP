d6ebe3386d85e90c4b61651fc9635fc95a454c21
Sidhant Chadha <sidhantchadha@live.com>
2018-02-05 16:39:15 -0600

    Add files via upload

---
+++ LL.h
@@ -0,0 +1,36 @@
+/*
+ * LL.h
+ *
+ *  Created on: Feb 3, 2018
+ *      Author: Powerlifter
+ */
+
+#ifndef LL_H_
+#define LL_H_
+
+#include <iostream>
+using namespace std;
+
+class NumberList {
+private:
+	struct ListNode {
+		int value;
+		ListNode *next;
+	};
+	ListNode *head;
+public:
+	NumberList() {
+		head=NULL;
+	}
+	~NumberList();
+	void appendNode(int);
+	void display();
+	void deleteNode(int);
+	void insertNode(int);
+	void reverseList();
+};
+
+
+
+
+#endif /* LL_H_ */
